a4b37b6b644b3493248728c3017c76980727bef9
Alaa Shatti <alaashatti91@gmail.com>
2019-07-22 19:03:53 +0300

    22-07-2019

---
+++ exam5.java
@@ -0,0 +1,14 @@
+exam5.java is ready
+wq
+second line
+3 line
+echo   >> 
+echo what are you need to write  >> 
+
+echo what do you want to write but in double quotation  >> 
+echo what do you want to write but in double quotation  >> txt name 
+{
+cat >> txt name --> Enter --> you are in txt folder 
+write everything here (in txt folder)
+if you want to exit on this txt just click control+c
+}
